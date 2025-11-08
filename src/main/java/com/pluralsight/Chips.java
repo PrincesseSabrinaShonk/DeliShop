8a801a336d816bcb9f52c3b8c90f388505fc8e3a
Sabrina D. <sabrinadorzin103@gmail.com>
2025-11-08 10:23:44 -0500

    Added comment to my chips class

--- src/main/java/com/pluralsight/Chips.java
+++ src/main/java/com/pluralsight/Chips.java
@@ -1,31 +1,35 @@
 package com.pluralsight;
 
-public class Chips extends OrderItems {  // --- Instance variables ---
+public class Chips extends OrderItems {        //Instance variables
     private String type;
     private static final double price = 1.50;   // All chips have the same price
 
-
+    // Constructor initializes the chip type
     public Chips(String type) {
         this.type = type;
     }
 
+    // Getter and setter for the chip type
     public String getType() {
         return type;
     }
-
     public void setType(String type) {
         this.type = type;
     }
+
+    // Override methods from OrderItems
     @Override
-    public double getPrice() {
+    public double getPrice() {     // Returns the fixed price for chips
         return price;
     }
 
+    // Returns a short description used for receipts or menus
     @Override
     public String getDescription() {
         return "";
     }
 
+    // Defines how the chip item appears as text 
     @Override
     public String toString() {
         return  String.format("%s Chips - $%.2f", type, price);
